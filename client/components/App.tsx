9460daa9beba1dc4144f882aa5ef9d08fa223062
Freddie Schultz <freddieschultz+devacademy@gmail.com>
2025-05-07 14:59:36 +1200

    Update homepage title

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,16 +1,9 @@
-import { useFruits } from '../hooks/useFruits.ts'
-
-function App() {
-  const { data } = useFruits()
-
+export default function App() {
   return (
     <>
       <div className="app">
-        <h1>Fullstack Boilerplate - with Fruits!</h1>
-        <ul>{data && data.map((fruit) => <li key={fruit}>{fruit}</li>)}</ul>
+        <h1>The Fractory</h1>
       </div>
     </>
   )
 }
-
-export default App
