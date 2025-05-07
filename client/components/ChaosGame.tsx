6ff3b055eaa51ca978ecf13f8fd5779d5d30b507
Freddie Schultz <freddieschultz+devacademy@gmail.com>
2025-05-08 10:58:34 +1200

    Create draw function for Sierpinski triangle

--- client/components/ChaosGame.tsx
+++ client/components/ChaosGame.tsx
@@ -9,7 +9,45 @@ export default function ChaosGame() {
   }
 
   const handleDraw = (canvasContext: CanvasRenderingContext2D) => {
-    canvasContext.fillRect(30, 30, 10, 10)
+    type Point = [number, number]
+
+    const getRandomPoint = (): Point => {
+      const x = Math.floor(Math.random() * 1000)
+      const y = Math.floor(Math.random() * 1000)
+      return [x, y]
+    }
+
+    const halfwayToPoint = (currentPoint: Point, newPoint: Point): Point => {
+      const newX = (currentPoint[0] + newPoint[0]) / 2
+      const newY = (currentPoint[1] + newPoint[1]) / 2
+      return [newX, newY]
+    }
+
+    const drawPoint = (point: Point) => {
+      canvasContext.fillRect(point[0], point[1], 1, 1)
+    }
+
+    const point1 = [500, 100] as Point
+    const point2 = [100, 793] as Point
+    const point3 = [900, 793] as Point
+
+    let currentPoint = getRandomPoint()
+
+    for (let i = 0; i < 1000000; i++) {
+      drawPoint(currentPoint)
+      const rand = Math.floor(Math.random() * 3)
+      switch (rand) {
+        case 0:
+          currentPoint = halfwayToPoint(currentPoint, point1)
+          break
+        case 1:
+          currentPoint = halfwayToPoint(currentPoint, point2)
+          break
+        case 2:
+          currentPoint = halfwayToPoint(currentPoint, point3)
+          break
+      }
+    }
   }
 
   return (
